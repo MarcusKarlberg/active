77914502f77140277fa25bf31b4e575abd3060b0
markar7 <marcus.karlberg@knowit.se>
2022-05-20 14:53:05 +0200

    implemented swagger. Added App description and tagged ProviderController endpoint for clarity

--- src/main/java/se/mk/active/ActiveApplication.java
+++ src/main/java/se/mk/active/ActiveApplication.java
@@ -1,15 +1,18 @@
 package se.mk.active;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
+@OpenAPIDefinition(info = @Info(title = "Active API", version = "1.0",
+		description = "API for municipalities and local governments to create venues and events"))
 public class ActiveApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ActiveApplication.class, args);
 	}
-
 }
